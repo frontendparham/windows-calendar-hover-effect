ae9762286174001e4906f5af8b991f1b22cf40e6
Parham Tavakolian <tavakolianparham@gmail.com>
2023-04-27 18:11:32 +0330

    Add files via upload

---
+++ dist/index.a0fd6288.js
@@ -0,0 +1,2 @@
+"use strict";document.body.onmousemove=e=>{for(const t of document.getElementsByClassName("box")){const o=t.getBoundingClientRect(),s=e.clientX-o.left,n=e.clientY-o.top;t.style.setProperty("--mouse-x",`${s}px`),t.style.setProperty("--mouse-y",`${n}px`)}};
+//# sourceMappingURL=index.a0fd6288.js.map
